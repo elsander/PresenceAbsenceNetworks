fe3991ec82a828851e3c770f598c8c5e71c8006a
Liz Sander <liz.sander91@gmail.com>
2017-02-15 13:31:13 +0900

    restructured folders

---
+++ Code/RCode/SubsampleComte.R
@@ -0,0 +1,70 @@
+library(dplyr)
+library(stringr)
+
+addNAsComte <- function(comte, outfname){
+    nNA <- length(unique(comte[,'OBJECTID']))
+    comte2 <- as.data.frame(matrix(NA, nrow(comte) + nNA, ncol(comte)))
+    comte2[1,] <- comte[1,]
+
+    k <- 2
+    for(i in 2:nrow(comte)){
+        if(comte[i,1] != comte[i-1,1]){
+            comte2[k,] <- rep(NA, ncol(comte))
+            comte2[k+1,] <- comte[i,]
+            k <- k + 2
+        } else {
+            comte2[k,] <- comte[i,]
+            k <- k + 1
+        }
+    }
+    names(comte2) <- names(comte)
+
+    write.table(comte2, outfname, quote = FALSE, row.names = FALSE)
+}
+
+onePlotFiles <- function(comte, plots, setName){
+    for(p in plots){
+        onePlotData <- dplyr::filter(comte, OBJECTID == p) %>%
+            dplyr::select(-OBJECTID, -Period)
+        fname <- stringr::str_c('../../Data/Comte-data/Comte-',
+                                setName, '/Comte-', setName,
+                                '-', p, '.txt')
+        write.table(onePlotData, fname, row.names = FALSE,
+                    col.names = FALSE, quote = FALSE)
+    }
+}
+
+subsamplePlots <- function(){
+    set.seed(216584)
+
+    comte <- read.table('../../Data/Comte-data/Comte-Presence-Absence.txt',
+                        header = TRUE, stringsAsFactors = FALSE)
+
+    ## Remove Barbus_barbus, because it is piscivorous, but little info
+    ## was available for it in the literature
+    comte <- comte[,-which(names(comte) == 'Bab')]
+
+    allplots <- unique(comte$OBJECTID)
+    ##shuffle plot order
+    allplots <- sample(allplots, size = length(allplots), replace = FALSE)
+
+    ##split into training and test sets (60/40)
+    trainingplots <- allplots[1:476] ## 476
+    testplots <- allplots[477:length(allplots)] ## 315
+
+    trainingSet <- filter(comte, OBJECTID %in% trainingplots)
+    testSet <- filter(comte, OBJECTID %in% testplots)
+
+    system('mkdir ../../Data/Comte-data/Comte-training-final/')
+    system('mkdir ../../Data/Comte-data/Comte-test-final/')
+    
+    ## add NAs and write to table
+    addNAsComte(trainingSet,
+                '../../Data/Comte-data/Comte-training-final/Comte-all-training-final.txt')
+    addNAsComte(testSet,
+                '../../Data/Comte-data/Comte-test-final/Comte-all-test-final.txt')
+
+    ## write data from individual sites to the appropriate folder
+    onePlotFiles(comte, trainingplots, 'training-final')
+    onePlotFiles(comte, testplots, 'test-final')
+}
